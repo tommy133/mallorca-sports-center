ad821532becad578eb8a43118b27e9f7c5639bf0
Tomeu <tomestrany@gmail.com>
2023-05-20 15:37:00 +0200

    refactor:change to Albert json

--- js/instal.js
+++ js/instal.js
@@ -1,7 +1,7 @@
 var centers;
 
 const xhttp = new XMLHttpRequest();
-xhttp.open("GET", "json/centres_esportius.json", true);
+xhttp.open("GET", "json/Sport_Center.json", true);
 xhttp.send();
 xhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
