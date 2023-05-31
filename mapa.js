40b2a0362abaf29c0083a91c8b52a54bc1de931a
AxelMP99 <41576074+AxelMP99@users.noreply.github.com>
2023-05-31 23:19:09 +0200

    Add files via upload

---
+++ mapa.js
@@ -0,0 +1,317 @@
+let map;
+let selectedMarker;
+let markers = [];
+let hotelDistances = [];
+let restauranteDistances = [];
+let gymDistances = [];
+
+async function initMap() 
+{
+  const { Map, InfoWindow, Icon} = await google.maps.importLibrary("maps");
+  map = new Map(document.getElementById("map"), {
+    center: { lat: 39.62, lng: 2.93 },
+    zoom: 10,
+  });
+
+  const infoWindow = new InfoWindow(); // InfoWindow para mostrar la foto
+
+   const iconosDeporte = 
+   {
+    Escalada: "svg/escalada.svg",
+    Basket: "svg/basket.svg",
+    Pàdel: "svg/padel.svg",
+  };
+
+  // Carga el archivo JSON
+  fetch("json/Sport_Center.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.itemListElement.forEach((element) => {
+        const latitude = element.geo.latitude;
+        const longitude = element.geo.longitude;
+        const deporte = element.tipusE;
+        const foto = element.image;
+        const nom = element.name;
+
+        const iconUrl = iconosDeporte[deporte];
+
+        // Crea el icono SVG personalizado
+        const icon = {
+          url: iconUrl,
+          scaledSize: new google.maps.Size(20, 20),
+          anchor: new google.maps.Point(0, 0),
+        };
+
+        const marker = new google.maps.Marker({
+          position: { lat: latitude, lng: longitude },
+          map: map,
+          icon: icon,
+        });
+
+        marker.addListener("click", () => {
+          selectedMarker = marker;
+          const content = `
+            <div>
+              <img src="${foto}" alt="Foto" style="width: 200px;">
+              <h5>${nom}</h5>
+              <p>${deporte}</p>
+              <a href="#" onclick="hotelesMasCerca('${marker.getPosition().toString()}')">Hotels més aprop</a><br>
+              <a href="#" onclick="restaurantesMasCerca('${marker.getPosition().toString()}')">Restaurants més aprop</a><br>
+              <a href="#" onclick="gymMasCerca('${marker.getPosition().toString()}')">Gimnassos més aprop</a>
+            </div>
+          `;
+          infoWindow.setContent(content);
+          infoWindow.open(map, marker);
+        });
+
+        const deporteSelect = document.getElementById("deporte-select");
+        deporteSelect.addEventListener("change", function () {
+          deporteSelect.value == deporte ? marker.setMap(map) : marker.setMap(null);
+        });
+        markers.push(marker); //Añadir marcador al array
+      });
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });
+}
+
+async function hotelesMasCerca(selectedMarkerPosition) 
+{
+  markers.forEach((marker) => {
+    if (marker.getPosition().toString() !== selectedMarkerPosition) {
+      marker.setMap(null); // Eliminar marcadores diferentes al seleccionado
+    }
+  });
+
+  const { InfoWindow } = await google.maps.importLibrary("maps");
+  const infoWindow = new InfoWindow();
+
+  fetch("json/hotel.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.itemListElement.forEach((element) => {
+        const latitude = parseFloat(element.geo.latitude);
+        const longitude = parseFloat(element.geo.longitude);
+        const foto = element.image;
+        const nom = element.name;
+
+        const icon = {
+          url: "svg/hotel.svg",
+          scaledSize: new google.maps.Size(20, 20), 
+          anchor: new google.maps.Point(0, 0), 
+        };
+
+        const markerHotel = new google.maps.Marker({
+          position: { lat: latitude, lng: longitude },
+          map: map,
+          icon: icon, 
+        });
+
+        const hotelDistance = {
+          name: nom,
+          distance: calculateDistance(selectedMarker,markerHotel),
+          marker: markerHotel
+        }
+
+        hotelDistances.push(hotelDistance);
+
+        markerHotel.addListener("click", () => {
+          const content = `
+            <div>
+              <img src="${foto}" alt="Foto" style="width: 200px;">
+              <h5>${nom}</h5>
+            </div>
+          `;
+          infoWindow.setContent(content);
+          infoWindow.open(map, markerHotel);
+        });
+      });
+      // Sort the array by distance
+    hotelDistances.sort(compareDistance);
+
+    //una vegada ordenats mos petam es markers menos es més propers
+
+    for (let i = 5; i < hotelDistances.length; i++) {
+      hotelDistances[i].marker.setMap(null); 
+    }
+    
+    
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });
+
+    
+    
+}
+
+async function restaurantesMasCerca(selectedMarkerPosition) 
+{
+  markers.forEach((marker) => {
+    if (marker.getPosition().toString() !== selectedMarkerPosition) {
+      marker.setMap(null); // Eliminar marcadores diferentes al seleccionado
+    }
+  });
+
+  const { InfoWindow } = await google.maps.importLibrary("maps");
+  const infoWindow = new InfoWindow();
+
+  fetch("json/restaurante.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.itemListElement.forEach((element) => {
+        const latitude = parseFloat(element.geo.latitude);
+        const longitude = parseFloat(element.geo.longitude);
+        const foto = element.image;
+        const nom = element.name;
+
+        const icon = {
+          url: "svg/restaurante.svg",
+          scaledSize: new google.maps.Size(20, 20), 
+          anchor: new google.maps.Point(0, 0), 
+        };
+
+        const markerRestaurante = new google.maps.Marker({
+          position: { lat: latitude, lng: longitude },
+          map: map,
+          icon: icon, 
+        });
+
+        const restauranteDistance = {
+          name: nom,
+          distance: calculateDistance(selectedMarker,markerRestaurante),
+          marker: markerRestaurante
+        }
+
+        restauranteDistances.push(restauranteDistance);
+
+        markerRestaurante.addListener("click", () => {
+          const content = `
+            <div>
+              <img src="${foto}" alt="Foto" style="width: 200px;">
+              <h5>${nom}</h5>
+            </div>
+          `;
+          infoWindow.setContent(content);
+          infoWindow.open(map, markerHotel);
+        });
+      });
+      // Sort the array by distance
+    restauranteDistances.sort(compareDistance);
+
+    //una vegada ordenats mos petam es markers menos es més propers
+
+    for (let i = 5; i < restauranteDistances.length; i++) {
+      restauranteDistances[i].marker.setMap(null); 
+    }
+    
+    
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });   
+    
+}
+
+async function gymMasCerca(selectedMarkerPosition) 
+{
+  markers.forEach((marker) => {
+    if (marker.getPosition().toString() !== selectedMarkerPosition) {
+      marker.setMap(null); // Eliminar marcadores diferentes al seleccionado
+    }
+  });
+
+  const { InfoWindow } = await google.maps.importLibrary("maps");
+  const infoWindow = new InfoWindow();
+
+  fetch("json/gym.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.itemListElement.forEach((element) => {
+        const latitude = parseFloat(element.geo.latitude);
+        const longitude = parseFloat(element.geo.longitude);
+        const foto = element.image;
+        const nom = element.name;
+
+        const icon = {
+          url: "svg/gym.svg",
+          scaledSize: new google.maps.Size(20, 20), 
+          anchor: new google.maps.Point(0, 0), 
+        };
+
+        const markerGym = new google.maps.Marker({
+          position: { lat: latitude, lng: longitude },
+          map: map,
+          icon: icon, 
+        });
+
+        const gymDistance = {
+          name: nom,
+          distance: calculateDistance(selectedMarker,markerGym),
+          marker: markerGym
+        }
+
+        gymDistances.push(gymDistance);
+
+        markerGym.addListener("click", () => {
+          const content = `
+            <div>
+              <img src="${foto}" alt="Foto" style="width: 200px;">
+              <h5>${nom}</h5>
+            </div>
+          `;
+          infoWindow.setContent(content);
+          infoWindow.open(map, markerGym);
+        });
+      });
+      // Sort the array by distance
+    gymDistances.sort(compareDistance);
+
+    //una vegada ordenats mos petam es markers menos es més propers
+
+    for (let i = 5; i < gymDistances.length; i++) {
+      gymDistances[i].marker.setMap(null); 
+    }
+    
+    
+    })
+    .catch((error) => {
+      console.error("Error al cargar el archivo JSON:", error);
+    });  
+    
+}
+
+
+// Function to calculate the distance between two markers
+function calculateDistance(marker1, marker2) {
+  const lat1 = marker1.getPosition().lat();
+  const lng1 = marker1.getPosition().lng();
+  const lat2 = marker2.getPosition().lat();
+  const lng2 = marker2.getPosition().lng();
+
+  const R = 6371; // Radius of the earth in kilometers
+  const dLat = deg2rad(lat2 - lat1); // Convert latitude difference to radians
+  const dLon = deg2rad(lng2 - lng1); // Convert longitude difference to radians
+
+  // Haversine formula
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distance in kilometers
+
+  return distance;
+}
+
+// Function to convert degrees to radians
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
+}
+
+// Custom comparison function for sorting by distance
+function compareDistance(a, b) {
+  return a.distance - b.distance;
+}
+
+initMap();
